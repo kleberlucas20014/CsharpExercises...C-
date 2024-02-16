244faf68f2e11ba919e7b9a2c5777c1f653695b8
kleber lucas <117679545+kleberlucas20014@users.noreply.github.com>
2024-02-16 06:05:48 +0000

    Add files via upload

---
+++ Classes e Objetos/Carro.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+
+
+{
+       
+     Console.WriteLine("### Classes e Objetos ###");
+
+
+    public class Carro
+    {
+        public string? Modelo;
+        public string? Montadora;
+        public string? Marca;
+        public int Ano;
+        public int Potencia;
+
+        public void Acelerar();
+        { 
+           Console.WriteLine("\nAcelerando...");
+        }
+    }
+
+
+Carro Chevrolet = new Carro();
+Chevrolet.Modelo = "Sedan";
+Chevrolet.Montadora = "Chevrolet";
+Chevrolet.Marca = "Onix";
+Chevrolet.Ano = 2016;
+Chevrolet.Potencia = 110;
+
+Chevrolet.Acelerar();
+Console.WriteLine($"{Chevrolet.Modelo} {Chevrolet.Montadora} {Chevrolet.Marca} {Chevrolet.Marca} {Chevrolet.Ano} {Chevrolet.Potencia}");
+
+
+
+Carro ford = new();
+ford.Modelo = "SUV";
+ford.Montadora = "Ford";
+ford.Marca = "Ecosport";
+ford.Ano = 2018;
+ford.Potencia = 120;
+
+Chevrolet.Acelerar();
+
+Console.WriteLine($"`{Chevrolet.Modelo}{Chevrolet.Montadora}{Chevrolet.Marca}{Chevrolet.Ano}{Chevrolet.Potencia}");
+
+Console.ReadKey();
+
+Console.WriteLine("\n################################################################################");
+
+
+}
---
+++ Classes e Objetos/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Criando Construtor (usando this)");
+
+        public class Carro
+        {  
+           public string? Modelo;
+           public string? Montadora;
+           public string? Marca;
+           public int Ano;
+           public int Potencia;
+
+          public Carro(string? Modelo, string? Montadora, string? Marca, int Ano, int Potencia); 
+          { 
+            this.Modelo = Modelo;
+            this.Montadora = Montadora;
+            this.Marca = Marca;
+            this.Ano = Ano;
+            this.Potencia = Potencia;
+
+          }
+
+           public void Acelerar(string marca);
+           {
+              Console.WriteLine("\n Acelerando o meu " + marca);
+           }
+
+
+
+    Console.WriteLine("criando objetos usando o construtor, exibir atributos e invocar o metodo");
+
+
+    Carro chevrolet = new Carro("Sedan", "Chevrolet", "Onix", 2016, 110);
+
+   chevrolet.Acelerar(chevrolet.Marca);
+
+   Console.WriteLine($"{chevrolet.Modelo} {chevrolet.Montadora} {chevrolet.Marca} {chevrolet.Ano} {chevrolet.Potencia}");
+
+   Carro ford = new("SUV", "Ford", "EcoSport", 2018, 120);
+
+    ford.Acelerar(ford.Marca);
+ 
+   Console.WriteLine($"{chevrolet.Modelo}{chevrolet.Montadora}{chevrolet.Marca}{chevrolet.Ano}{chevrolet.Potencia} CV");
+
+   Console.ReadKey();
+      
+ 
+        } 
+
+
+
+    }
+
+
+}
+    
+   
